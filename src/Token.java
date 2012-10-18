39272cad331bcdbb258a8f13ef7c268111ef79e7
Alfonso Vazquez <fonzi.tk@gmail.com>
2012-10-18 13:38:40 -0500

    Token java file v.1

---
+++ src/Token.java
@@ -0,0 +1,52 @@
+/**
+ * enum called token to list all the lexical units from the small pascal language
+ * 
+ * @author Alfonso Vazquez
+ *
+ */
+public enum Token 
+{
+	
+	
+	//Reserved Words
+	IF,
+	THEN,
+	ELSE,
+	WHILE,
+	DO,
+	VAR,
+	BEGIN,
+	END,
+	ARRAY,
+	OF,
+	FUNCTION,
+	PROCEDURE,
+	OR,
+	NOT,
+	MOD,
+	AND,
+	REAL,
+	INTEGER,
+	ID,
+	
+	//Other units | Symbols.
+	SEMI_COLON,
+	PERIOD,
+	PERIOD_EQUALS,
+	LEFT_SQUARE_BRACKET,
+	RIGHT_SQUARE_BRACKET,
+	LEFT_PARENTHESIS,
+	RIGHT_PARENTHESIS,
+	PLUS,
+	MINUS,
+	MULTIPLY,
+	DIVIDE,
+	EQUAL,
+	NOT_EQUAL,
+	LESS_THAN,
+	LESS_THAN_EQUAL,
+	GREATER_THAN_EQUAL,
+	GREATER_THAN,
+	COLON,
+	
+}
