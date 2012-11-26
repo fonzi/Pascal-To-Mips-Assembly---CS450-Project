2142cb26045415842cf5f10a88569df06c5ce4c5
fonzi_vazquez <vazquez@augsburg.edu>
2012-11-25 22:13:20 -0600

    executable V2(build 1): now with the parser implementations

--- src/Executable.java
+++ src/Executable.java
@@ -19,9 +19,12 @@ public class Executable
             }
             int i = 0;
             File file = new File(args[0]);
+            String fileName = args[0];
             SymbolTable table = new SymbolTable();
             CompilerScanner scan = new CompilerScanner(file,table);
-            CompilerParser parse = new CompilerParser();
+            CompilerParser parse = new CompilerParser(fileName);
+            parse.program();
+            
             while (i != 1)
             {
                 i = scan.nextToken();
