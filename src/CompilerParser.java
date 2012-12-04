dd8c64a80ced597944e0d5d057dd1aabc09a62f1
Alfonso Vazquez <vazquez@IMACY11-3GDHJR.(none)>
2012-12-04 11:45:24 -0600

    This is updated version of parser, with more javadocs and more method implemented

--- src/CompilerParser.java
+++ src/CompilerParser.java
@@ -13,7 +13,7 @@ import java.util.Hashtable;
 */
 
 public class CompilerParser
-{	/*
+{	/**
 	* This private Global variable is of Type CompilerScanner 
 	*/
 	private CompilerScanner scanner;
@@ -22,8 +22,11 @@ public class CompilerParser
 	*/
     private Token currentToken;
 
-	/*
-	*      
+	/**
+	* CompilerParser method, initializes the Scanner, Hashtable for the symbolTable
+	* Also calls the currentToken to get the token
+	*
+	* @param filename of type String to initialize the file input
 	*/
     public CompilerParser(String filename)
     {
@@ -33,7 +36,12 @@ public class CompilerParser
         scanner.nextToken();
     	currentToken = scanner.getToken();
     }
-        
+    /**
+	* Program method is the first call the file will make 
+	* It will match three things and then call 
+	* declarations > subprogram_declarations > compound_statement 
+	* and then it would match again.
+    */   
     public void program()
     {
         System.out.println("program");
@@ -45,7 +53,10 @@ public class CompilerParser
         compound_statement();
         match(currentToken.PERIOD);//match the .
     }
-    
+    /**
+    * Identifier list matches an id or matches and id and calls 
+    * identifier_list
+    */
     public void identifier_list()
     {
 		System.out.println("identifier_list");
@@ -59,7 +70,11 @@ public class CompilerParser
 			identifier_list();
 		}
 	}
-	
+	/**
+	* declarations matches var and calls
+	* identifier_list > matches > type > matches > declarations
+	*
+	*/
 	public void declarations()
 	{
 		System.out.println("declerations");
@@ -71,6 +86,12 @@ public class CompilerParser
 		declarations();
 	}
 	
+	/**
+	* type if token = array then 
+	* match "Array" and "[" and "Number" and ":" and "Number" and "]" and "of"
+	* then calls standard_type
+	* or just matches standard_type
+	*/
 	public void type()
 	{
 		System.out.println("type");
@@ -91,6 +112,9 @@ public class CompilerParser
 		}
 	}
 	
+	/**
+	* Standard_type matches either Number or Real only
+	*/
 	public void standard_type()
 	{
 		System.out.println("standard_type");
@@ -104,9 +128,13 @@ public class CompilerParser
 		}
 	}
 	
+	/**
+	* subprogram_declarations calls itself, then if currentToken is ";"
+	* it calls itself again 
+	*/
 	public void subprogram_declarations()
 	{
-		System.out.println("subprogram_declerations");
+		System.out.println("subprogram_declarations");
 		subprogram_declaration();
 		if(currentToken == currentToken.SEMI_COLON)
 		{
@@ -116,6 +144,10 @@ public class CompilerParser
 			currentToken = null;//null == lambda
 	}
 	
+	/**
+	* subprogram_declaration calls 
+	* subprogram_head > declarations > subprogram_declarations > compound_statement
+	*/
 	public void subprogram_declaration()
 	{
 		System.out.println("subprogram_declarations");
@@ -124,12 +156,12 @@ public class CompilerParser
 		subprogram_declarations();
 		compound_statement();
 	}
-	
-	public void compound_declarations()
-	{
-		System.out.println("compound_declarations");
-	}
-	
+
+	/**
+	* subprogram_head matches and calls 
+	* if currentToken is Function the 
+	* match function and match id call arguments and match ":" and call standard_type
+	*/
 	public void subprogram_head()
 	{
 		System.out.println("subprogram_head");
@@ -149,6 +181,11 @@ public class CompilerParser
 		}
 	}
 	
+	/**
+	* arguments matches if currentToken is "("
+	* matches "(" calls parameter_list, match ")"
+	* 
+	*/
 	public void arguments()
 	{
 		System.out.println("arguments");
@@ -162,11 +199,36 @@ public class CompilerParser
 			currentToken = null;
 	}
 	
+	/**
+	* parameter_list calls identifier_list > matches colon > type 
+	* or identifier_list > match ":" > type > match ";" > parameter_list  
+	*/
 	public void parameter_list()
 	{
 		System.out.println("parameter_list");
-	}
+		if(currentToken == Token.ID)
+		{
+			identifier_list();
+			match(currentToken.COLON);
+			type();
+		}
+		else
+		{
+			if(currentToken = Token.ID)
+			{
+				identifier_list();
+				match(currentToken.COLON);
+				type();
+				match(SEMI_COLON);
+				parameter_list();
+			}//end if
+		}//end else
+	} 
 	
+	/**
+	* compound_statement matches begin and end with 
+	* optional_statements in between been called
+	*/
 	public void compound_statement()
 	{
 		System.out.println("compound_statement");
@@ -175,25 +237,46 @@ public class CompilerParser
 		match(currentToken.END);
 	}
 	
+	/**
+	* optional_statement calls statement_list if currentToken is and ID
+	* or lambda
+	*/
 	public void optional_statements()
 	{
 		System.out.println("optional_statements");
-		if(currentToken != null)
+		if(currentToken = Token.ID)
 		{
 			statement_list();
 		}
-		else
-			currentToken = null;
 	}
-	
+	/**
+	* calls statement or calls statement matches ";" and calls statement_list
+	*/
 	public void statement_list()
 	{
 		System.out.println("statement_list");
+		if(currentToken = Token.ID
+		{
+			statement();
+		}
+		else
+		{
+			statement();
+			match(currentToken.SEMI_COLON)//match ;
+			statement_list();
+		}
 	}
 	
+	/**
+	* 
+	*/
 	public void statement()
 	{
 		System.out.println("statement");
+		if(currentToken = Token.ID)
+		{
+			
+		}
 	}
 	
 	public void variable()
